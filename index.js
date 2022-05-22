6f633309e31b4b9894233b2709e067ffb5a4026c
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-23 00:06:51 +0600

    connected with databse and load the database

--- index.js
+++ index.js
@@ -12,7 +12,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdlgt.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const partCollection = client.db('keyboardquipo').collection('parts');
+
+    app.get('/part', async(req, res) => {
+      const query = {};
+      const cursor = partCollection.find(query);
+      const parts = await cursor.toArray();
+      res.send(parts);
+    })
+  }
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Keyboarquipo!')
