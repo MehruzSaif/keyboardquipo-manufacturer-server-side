34932275e1aae5b2caedecea3c0a56f9c96aaed2
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-26 05:34:56 +0600

    using middleware

--- index.js
+++ index.js
@@ -36,7 +36,18 @@ async function run() {
     const partCollection = client.db('keyboardquipo').collection('parts');
     const bookingCollection = client.db('keyboardquipo').collection('bookings');
     const userCollection = client.db('keyboardquipo').collection('users');
-    const equipmentCollection = client.db('keyboardquipo').collection('equipments');
+    /* const equipmentCollection = client.db('keyboardquipo').collection('equipments'); */
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'Forbidden access' })
+      }
+    }
 
     // read all data of parts
     app.get('/part', async (req, res) => {
@@ -81,28 +92,21 @@ async function run() {
     })
 
     // only for admin
-    app.get('/admin/:email', async(req, res) => {
+    app.get('/admin/:email', async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({email: email});
+      const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === 'admin';
-      res.send({admin: isAdmin});
+      res.send({ admin: isAdmin });
     })
 
-    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({ email: requester });
-      if (requesterAccount.role === 'admin') {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: 'admin' },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      }
-      else {
-        res.status(403).send({ message: 'forbidden access' });
-      }
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
 
     });
 
@@ -121,9 +125,9 @@ async function run() {
 
 
     // for addPart collection for admin
-    app.post('/equipment', async(req, res) => {
+    app.post('/part', verifyJWT, verifyAdmin, async (req, res) => {
       const equipment = req.body;
-      const result = await equipmentCollection.insertOne(equipment);
+      const result = await partCollection.insertOne(equipment);
       res.send(result);
     })
 
