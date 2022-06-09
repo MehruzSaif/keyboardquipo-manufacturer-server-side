a6f4d82cd8a324eb75b0426dd22a6cbf2a3b8cb1
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-06-09 22:15:47 +0600

    msg

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({origin: 'https://keyboardquipo.web.app/'}))
+app.use(cors());
 app.use(express.json());
 
 
